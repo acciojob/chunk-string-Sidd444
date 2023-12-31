ca84b29befeeee48010ea19c97205f6b3e464280
Sidd444 <123827703+Sidd444@users.noreply.github.com>
2023-12-31 19:53:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 function stringChop(str,size) {
   // your code here
-  let curr=0,arr=[];
+   let curr=0,arr=[];
   size=parseInt(size);
-  while(true){
-	 if(size+curr<=str.length){
-		 arr.push(str.substring(curr,size+curr));
-	     curr=curr+size;
-      }else break;
+  while(curr<str.length){
+     arr.push(str.substring(curr,Math.min(size+curr, str.length)));
+     curr=curr+size;
   }
-  if(curr<str.length) arr.push(str.substring(curr,str.length));
   return arr;
 }
   
